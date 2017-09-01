026a4c75ee2b26a893dc6c523d0d3d76add3cda4
Freed <flyxz@126.com>
2017-09-01 18:24:31 +0800

    改writeFlie为appendFlie

--- getYyjw.js
+++ getYyjw.js
@@ -32,30 +32,32 @@ function getChapter(chapter){
 			}else {
 				chapterTitle.isWriteFile = false
 			}
-			//读取原内容，防止覆盖
-			oldText = fs.readFileSync('yyjw.txt','utf8');
 			//规范标题
 			cleanTitle = chapterTitle.isWriteFile? chapterTitle.name.replace(/(章)|(\s+)/g,function(macth,p1,p2){
 							if (p1) return '第';
 							if (p2) return '章\r\n';
 						}):'';
 			//规范内容，去除冗余
-			cleanContent = rawData.result.content.replace(/(（本章未完，请翻页）)|(（本章完）|(\<p\>)|(\<\/p\>))/g,function(macth,p1,p2,p3,p4){
+			cleanContent = rawData.result.content?rawData.result.content.replace(/(（本章未完，请翻页）)|(（本章完）|(\<p\>)|(\<\/p\>))/g,function(macth,p1,p2,p3,p4){
 				if (p1) return '';
 				if (p2) return '';
 				if (p3) return '';
 				if (p4) return '\r\n';
-			});
+			}):'本章没有抓取到内容';
 			// 写入文件
-			fs.writeFile('yyjw.txt', oldText+cleanTitle+cleanContent, 'utf8', res => {
+			fs.appendFile('yyjw.txt',cleanTitle+cleanContent, 'utf8', err => {
+				if (err){
+					throw err
+				}else {
 					console.log(chapterNum);
 					chapterNum++;
 					// 不知道最终章的判断，只能手动区获取最终章的id
 					if (rawData.result.nextChapterId != lastChapter){
 						setTimeout(function(){
 							getChapter(rawData.result.nextChapterId)
-						},1000);
+						},2000);
 					}
+				}
 			});
 		});
 	});
