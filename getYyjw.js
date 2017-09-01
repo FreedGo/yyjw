a85c0a8d4f64a2969b2960f74ae273fa9075c260
Freed <flyxz@126.com>
2017-09-01 17:58:41 +0800

    优化部分处理

--- getYyjw.js
+++ getYyjw.js
@@ -6,18 +6,15 @@
 const fs = require('fs');
 const http = require('http');
 let chapterNum = 1; //控制循环
-
 let lastChapter = '38692140_2';//最终章节
-
 let NextChapter = '5768909';//初始章节
-
 let chapterTitle = {
 	name :'',
 	isWriteFile:false
 };//因为一章被分成三段，用以三个章名合一
-
-let oldText = '';
-
+let oldText = '';//读取原内容
+let cleanTitle = '';//正则清洗过的标题
+let cleanContent = '';//正则清洗过的内容
 function getChapter(chapter){
 	//发送请求数据
 	let params = 'bookId=342974&chapterId='+chapter+'&v=1504248913910';
@@ -37,26 +34,27 @@ function getChapter(chapter){
 			}
 			//读取原内容，防止覆盖
 			oldText = fs.readFileSync('yyjw.txt','utf8');
+			//规范标题
+			cleanTitle = chapterTitle.isWriteFile? chapterTitle.name.replace(/(章)|(\s+)/g,function(macth,p1,p2){
+							if (p1) return '第';
+							if (p2) return '章\r\n';
+						}):'';
+			//规范内容，去除冗余
+			cleanContent = rawData.result.content.replace(/(（本章未完，请翻页）)|(（本章完）|(\<p\>)|(\<\/p\>))/g,function(macth,p1,p2,p3,p4){
+				if (p1) return '';
+				if (p2) return '';
+				if (p3) return '';
+				if (p4) return '\r\n';
+			});
 			// 写入文件
-			fs.writeFile(
-				'yyjw.txt',
-				oldText+(chapterTitle.isWriteFile? chapterTitle.name.replace(/(章)|(\s)/g,function(macth,p1,p2){
-						if (p1) return '第';
-						if (p2) return '章\r\n';
-						 }):'')+rawData.result.content.replace(/(（本章未完，请翻页）)|(（本章完）|(\<p\>)|(\<\/p\>))/g,function(macth,p1,p2){
-					if (p1) return '';
-					if (p2) return '';
-					if (p3) return '';
-					if (p4) return '\r\n';
-				}),
-				'utf8',
-				res => {
+			fs.writeFile('yyjw.txt', oldText+cleanTitle+cleanContent, 'utf8', res => {
 					console.log(chapterNum);
 					chapterNum++;
+					// 不知道最终章的判断，只能手动区获取最终章的id
 					if (rawData.result.nextChapterId != lastChapter){
 						setTimeout(function(){
 							getChapter(rawData.result.nextChapterId)
-							},1000);
+						},1000);
 					}
 			});
 		});
